7ca5561d2470e6dc8b66e704947109c8d8a9f0cb
timmyreilly <timreilly@live.com>
2015-12-02 10:39:26 -0800

    initial commit

---
+++ src/editor.ts
@@ -0,0 +1,54 @@
+import {window, workspace, commands, Disposable, ExtensionContext, TextDocument} from 'vscode';
+import * as vscode from 'vscode';
+import AudioPlay from './extension';
+
+export default class EditorObserver {
+    private _disposable: Disposable;
+    private _audioplayer: AudioPlay;
+    private _lastText : any;
+    private _lastLine: number;
+    
+    constructor(audioplayer: AudioPlay) {
+        this._audioplayer = audioplayer;
+        
+        // subscribe to selection change and editor activation events...
+        let subscriptions: Disposable[] = [];
+        window.onDidChangeTextEditorSelection(this._onEvent, this, subscriptions);
+        window.onDidChangeActiveTextEditor(this._onEvent, this, subscriptions);
+        
+        this._disposable = Disposable.from(...subscriptions);
+    }
+    
+    private _getLetterCount(doc: TextDocument): number {
+        return doc.getText().split("").length;
+    }
+    
+    private _hasEnteredReturn(e: vscode.TextEditorSelectionChangeEvent): boolean {
+        // selection[0] is the start, and selection[1] is the end
+        // Taken from the event, which contains the current texteditor and its selections
+		let lastPosition : vscode.Selection = e.textEditor.selections[0];
+        let lineChanged = (lastPosition.end.line !== this._lastLine);
+            
+        this._lastLine = lastPosition.end.line;
+        return lineChanged;
+    }
+    
+    private _hasTextChanged(e: vscode.TextEditorSelectionChangeEvent) {
+        let currentText = e.textEditor.document.getText();
+        let hasChangd = (currentText !== this._lastText);
+        this._lastText = currentText;
+        
+        return hasChangd;
+    }
+    
+    private _onEvent(e: vscode.TextEditorSelectionChangeEvent) {
+        if (!this._hasTextChanged(e)) return; 
+        let lineChanged = this._hasEnteredReturn(e);
+        
+        if (lineChanged) {
+            this._audioplayer.playCarriageReturn();
+        } else {
+            this._audioplayer.playKeystroke();
+        }
+    }
+}
---
+++ src/extension.ts
@@ -0,0 +1,54 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import {window, workspace, commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument} from 'vscode';
+import EditorObserver from './editor';
+import cp = require('child_process');
+
+var path = require("path");
+var player = require('play-sound')({});
+
+// this method is called when your extension is activated. activation is
+// controlled by the activation events defined in package.json
+
+var letterCounter, audioPlayer, controller;
+
+export function activate(ctx: ExtensionContext) {
+
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "WordcountTester" is now active!');
+    
+    audioPlayer = audioPlayer || new AudioPlay();
+    controller = controller || new EditorObserver(audioPlayer);
+
+    // add to a list of disposables which are disposed when this extension
+    // is deactivated again.
+    ctx.subscriptions.push(controller);
+}
+
+export class AudioPlay {
+    private _keypress_path:string = path.join(__dirname, '..', '..', 'audio', 'typewriter-key-1.wav');
+    private _playExe_path:string = path.join(__dirname, '..', '..', 'audio', 'play.exe');
+    private _carriagereturn_path:string = path.join(__dirname, '..','..','audio','typewriter-return-1.wav');
+    private _isWindows:boolean;
+    
+    public playKeystroke () {
+        if (this._isWindows) {
+            cp.execFile(this._playExe_path, [this._keypress_path]);
+        } else {
+            player.play(this._keypress_path);
+        }
+    }
+    
+    public playCarriageReturn () {
+        if (this._isWindows) {
+            cp.execFile(this._playExe_path, [this._carriagereturn_path]);
+        } else {
+            player.play(this._carriagereturn_path);
+        }
+    }
+    
+    constructor(){
+        this._isWindows = (process.platform === 'win32');
+    }
+}
